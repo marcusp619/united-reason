55290d88c1ed019ebb33174088c3f1e9340fe7df
marcusp619 <marcusp619@gmail.com>
2024-01-20 19:16:36 -0500

    feat: update base styles

---
+++ src/lib/Header.svelte
@@ -0,0 +1,22 @@
+<header class="py-10">
+ <div>
+    <nav class="relative z-50 flex justify-between">
+        <div class="flex items-center md:gap-x-12">
+            <a href="/" class="flex items-center">
+                <img src="#" alt="logo" class="h-10 w-auto">
+                <span class="ml-2 text-2xl font-bold text-gray-800">SvelteKit</span>
+            </a>
+            <div class="hidden md:flex md:gap-x-6">
+                <a href="#features" class="ml-12 text-gray-600 hover:text-gray-800">Features</a>
+                <a href="#testimonials" class="ml-12 text-gray-600 hover:text-gray-800">Testimonials</a>
+                <a href="#pricing" class="ml-12 text-gray-600 hover:text-gray-800">Pricing</a>
+            </div>
+        </div>
+        <div class="flex items-center gap-x-5 md:gap-x-8">
+            <div class="hidden md:block">
+                <a href="/login"> Sign in</a>
+            </div>
+        </div>
+    </nav>
+ </div>
+</header>
