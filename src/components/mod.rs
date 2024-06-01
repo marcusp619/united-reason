63ab571744614648704d24d02552068582cc2842
Drake Bott <bott.da@gmail.com>
2024-06-01 14:00:08 -0500

    Friendship has ended with Svelte, Leptos is my new best friend

---
+++ src/components/mod.rs
@@ -0,0 +1,5 @@
+pub mod button;
+pub mod container;
+pub mod header;
+pub mod hero;
+pub mod primary_features;
--- src/lib/Container.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    export let className: string | undefined;
-</script>
-  
-<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 {className}" {...$$restProps}>
-    <slot />
-</div>
