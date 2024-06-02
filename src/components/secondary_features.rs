601414f72f8a052fd978b278174ac70ad91ccced
marcusp619 <marcusp619@gmail.com>
2024-06-02 17:21:15 -0400

    feat: init setup for secondary feat

---
+++ src/components/secondary_features.rs
@@ -0,0 +1,9 @@
+use leptos::{component, view, IntoView};
+use crate::components::container::Container;
+
+#[component]
+pub fn SecondaryFeatures() -> impl IntoView {
+    view! {
+        
+    }
+}
