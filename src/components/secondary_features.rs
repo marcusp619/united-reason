42c24c9ab13317f70244486896cd2d6bef4eb084
marcusp619 <marcusp619@gmail.com>
2024-06-02 22:53:10 -0400

    feat: setting up props and featuresmobile comp

--- src/components/secondary_features.rs
+++ src/components/secondary_features.rs
@@ -1,5 +1,6 @@
 use leptos::{component, view, IntoView};
 use crate::components::container::Container;
+use std::collections::HashMap;
 
 // Define the Feature interface to a struct
 struct Feature {
@@ -22,7 +23,7 @@ impl Feature {
 }
 
 // Define a vector(aka an array) of features
-let features: Vec<Feature> = vec![
+static features: Vec<Feature> = vec![
     Feature::new(
         "Reporting",
         "Stay on top of things with always up-to-date reporting features.",
@@ -44,13 +45,56 @@ let features: Vec<Feature> = vec![
 ]; 
 
 
+ 
+struct SecondaryFeatureProps {
+    feature: Feature,
+    is_active: bool,
+    class_name: String,
+    // recreating reacts ...props
+    attributes: HashMap<String, String>,
+}
+
+
 #[component]
-pub fn SecondaryFeatures() -> impl IntoView {
+pub fn SecondaryFeatures(props: SecondaryFeatureProps) -> impl IntoView {
     view! {
         <div
-            class=:
+            class{format!("{} {}", props.class_name, !props.is_active && "opacity-75 hover:opacity-100")}
+            {...props.attributes}
         >
-        
+            <div
+                class={format!("w-9 rounded-lg {}", if props.is_active "bg-blue-600" else "bg-slate-500" )}
+            >
+                <svg
+                    aria-hidden="true"
+                    class="h-9 w-9"
+                    fill="none"
+                >
+                    <props.feature.icon />
+                </svg>
+            </div>
+            <h3
+            class={format!("mt-6 text-sm font-medium", if props.is_active "text-blue-600" else "text-slate-500" )}
+            >
+                {props.feature.name}
+            </h3>
+            <p class=("mt-2 font-display text-xl text-slate-900")>
+                {props.feature.summary}
+            </p>
+        </div>
+    }
+}
+
+#[component]
+pub fn FeaturesMobile() -> impl IntoView {
+    view! {
+        <div class=("-mx-4 mt-20 flex flex-col gap-y-10 overflow-hidden px-4 sm:-mx-6 sm:px-6 lg:hidden")>
+            {features.into_iter().map(|feature| {
+                view! {
+                    <div>
+                    </div>
+                }
+            }).collect_view()}
         </div>
     }
 }
