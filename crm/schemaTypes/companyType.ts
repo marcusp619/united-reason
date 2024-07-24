c130cdeaba3282251bc0edb8d6ac25cd414e620f
marcusp619 <marcusp619@gmail.com>
2024-07-24 17:38:42 -0400

    refactor: update import declaration

--- crm/schemaTypes/companyType.ts
+++ crm/schemaTypes/companyType.ts
@@ -1,36 +1,36 @@
-import { defineField } from "sanity";
+import {defineField, defineType} from 'sanity'
 
-export default {
-    name: 'company',
-    title: 'Company',
-    type: 'document',
-    fields: [
-      defineField({
-        name: 'name',
-        title: 'Company Name',
-        type: 'string',
-        validation: Rule => Rule.required()
-      }),
-      defineField({
-        name: 'description',
-        title: 'Company Description',
-        type: 'text'
-      }),
-      defineField({
-        name: 'location',
-        title: 'Location',
-        type: 'reference',
-        to: [{ type: 'location' }]
-      }),
-      defineField({
-        name: 'website',
-        title: 'Website',
-        type: 'url'
-      }),
-      defineField({
-        name: 'logo',
-        title: 'Logo',
-        type: 'image'
-      })
-    ]
-  };
+export const companyType = defineType({
+  name: 'company',
+  title: 'Company',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Company Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'description',
+      title: 'Company Description',
+      type: 'text',
+    }),
+    defineField({
+      name: 'location',
+      title: 'Location',
+      type: 'reference',
+      to: [{type: 'location'}],
+    }),
+    defineField({
+      name: 'website',
+      title: 'Website',
+      type: 'url',
+    }),
+    defineField({
+      name: 'logo',
+      title: 'Logo',
+      type: 'image',
+    }),
+  ],
+})
--- crm/schemaTypes/jobCategoryType.ts
+++ crm/schemaTypes/jobCategoryType.ts
@@ -1,20 +1,20 @@
-import { defineField } from "sanity";
+import {defineField, defineType} from 'sanity'
 
-export default {
-    name: 'jobCategory',
-    title: 'Job Category',
-    type: 'document',
-    fields: [
-      defineField({
-        name: 'title',
-        title: 'Category Title',
-        type: 'string',
-        validation: Rule => Rule.required()
-      }),
-      defineField({
-        name: 'description',
-        title: 'Category Description',
-        type: 'text'
-      })
-    ]
-  };
+export const jobCategoryType = defineType({
+  name: 'jobCategory',
+  title: 'Job Category',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Category Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'description',
+      title: 'Category Description',
+      type: 'text',
+    }),
+  ],
+})
