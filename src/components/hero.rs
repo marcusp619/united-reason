c913ce48b91afc4a15f8e79fd389ffd49c9a0d60
marcusp619 <marcusp619@gmail.com>
2024-07-24 19:34:27 -0400

    wip

--- src/components/hero.rs
+++ src/components/hero.rs
@@ -1,5 +1,9 @@
-use leptos::{component, view, CollectView, IntoView};
-use crate::components::{container::Container, button::{ButtonLink, Variant}};
+use crate::components::{
+    button::{ButtonLink, Variant},
+    container::Container,
+};
+use leptos::{component, server, view, CollectView, IntoView, ServerFnError, Suspense};
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug)]
 struct Image {
@@ -9,79 +13,121 @@ struct Image {
 
 static HERO_IMAGES: &[&[Image]] = &[
     &[
-			Image { name: "Transistor", logo: "/images/logos/transistor.svg" },
-			Image { name: "Tuple", logo: "/images/logos/tuple.svg" },
-			Image { name: "StaticKit", logo: "/images/logos/statickit.svg" }
-		],
-		&[
-			Image { name: "Mirage", logo: "/images/logos/mirage.svg" },
-			Image { name: "Laravel", logo: "/images/logos/laravel.svg" },
-			Image { name: "Statamic", logo: "/images/logos/statamic.svg" }
-		]
+        Image {
+            name: "Transistor",
+            logo: "/images/logos/transistor.svg",
+        },
+        Image {
+            name: "Tuple",
+            logo: "/images/logos/tuple.svg",
+        },
+        Image {
+            name: "StaticKit",
+            logo: "/images/logos/statickit.svg",
+        },
+    ],
+    &[
+        Image {
+            name: "Mirage",
+            logo: "/images/logos/mirage.svg",
+        },
+        Image {
+            name: "Laravel",
+            logo: "/images/logos/laravel.svg",
+        },
+        Image {
+            name: "Statamic",
+            logo: "/images/logos/statamic.svg",
+        },
+    ],
 ];
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct ApiResponse {
+    data: String, // Adjust this according to the API response structure
+}
+
+#[server(FetchData, "/api", "GetJson")]
+pub async fn fetch_data() -> Result<ApiResponse, ServerFnError> {
+    let url = "https://ly6kxhrq.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+%3D%3D+%27jobPosting%27%5D"; // Replace with your API URL
+    let response = reqwest::get(url).await?.json::<ApiResponse>().await?;
+    println!("{:?}", response);
+
+    Ok(response)
+}
 
 #[component]
 pub fn Hero() -> impl IntoView {
+    let job_postings = fetch_data();
+    
     view! {
-    <Container class=String::from("pb-16 pt-20 text-center lg:pt-32")>
-      <h1 class="font-display mx-auto max-w-4xl text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
-        Talent Aquisition{' '}
-        <span class="relative whitespace-nowrap text-blue-600">
-          <svg
-            aria-hidden="true"
-            viewBox="0 0 418 42"
-            class="absolute left-0 top-2/3 h-[0.58em] w-full fill-blue-300/70"
-            preserveAspectRatio="none"
-          >
-            <path
-              d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z" />
-          </svg>
-          <span class="relative">made simple</span>
-        </span>{' '}
-        for your business.
-      </h1>
-      <p class="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
-        Most job seeking software is accurate, but hard to use. We make the opposite trade-off, and help
-        you gain new employees.
-      </p>
-      <div class="mt-10 flex justify-center gap-x-6">
-        <ButtonLink href=String::from("/register")>Get 6 months free</ButtonLink>
-        <ButtonLink href=String::from("https://youtu.be/sxjgL64czRY") variant=Variant::Outline>
-            <svg aria-hidden="true" class="h-3 w-3 flex-none fill-blue-600 group-active:fill-current">
-                <path d="m9.997 6.91-7.583 3.447A1 1 0 0 1 1 9.447V2.553a1 1 0 0 1 1.414-.91L9.997 5.09c.782.355.782 1.465 0 1.82Z" />
-            </svg>
-             <span class="ml-3">Watch video</span>
-        </ButtonLink>
-	</div>
-	<div class="mt-36 lg:mt-44">
-		<p class="font-display text-base text-slate-900">Trusted by these six companies so far</p>
-		<ul
-			role="list"
-			class="mt-8 flex items-center justify-center gap-x-8 sm:flex-col sm:gap-x-0 sm:gap-y-10 xl:flex-row xl:gap-x-12 xl:gap-y-0"
-		>
-    {HERO_IMAGES
-        .into_iter()
-        .map(|group| {
-            view! {
-            <li>
-                <ul
-                    role="list"
-                    class="flex flex-col items-center gap-y-8 sm:flex-row sm:gap-x-12 sm:gap-y-0">
-                    {group.into_iter()
-                        .map(|image| {
-                            view! {
-                              <li class="flex">
-                                <img src={image.logo} alt={image.name} />
-                              </li>
-                            }
-                        })
-                        .collect_view()}
-                </ul>
-            </li>
-            }})
-        .collect_view()}
-		</ul>
-	</div>
-</Container>
-    }
+      <Suspense fallback=|| ()>
+        <Container class=String::from("pb-16 pt-20 text-center lg:pt-32")>
+          <h1 class="font-display mx-auto max-w-4xl text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
+            Talent Aquisition{' '}
+            <span class="relative whitespace-nowrap text-blue-600">
+              <svg
+                aria-hidden="true"
+                viewBox="0 0 418 42"
+                class="absolute left-0 top-2/3 h-[0.58em] w-full fill-blue-300/70"
+                preserveAspectRatio="none"
+              >
+                <path
+                  d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z" />
+              </svg>
+              <span class="relative">made simple</span>
+            </span>{' '}
+            for your business.
+          </h1>
+          <p class="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
+            Most job seeking software is accurate, but hard to use. We make the opposite trade-off, and help
+            you gain new employees.
+          </p>
+          <div class="mt-10 flex justify-center gap-x-6">
+            <ButtonLink href=String::from("/register")>Get 6 months free</ButtonLink>
+            <ButtonLink href=String::from("https://youtu.be/sxjgL64czRY") variant=Variant::Outline>
+                <svg aria-hidden="true" class="h-3 w-3 flex-none fill-blue-600 group-active:fill-current">
+                    <path d="m9.997 6.91-7.583 3.447A1 1 0 0 1 1 9.447V2.553a1 1 0 0 1 1.414-.91L9.997 5.09c.782.355.782 1.465 0 1.82Z" />
+                </svg>
+                 <span class="ml-3">Watch video</span>
+            </ButtonLink>
+        </div>
+        <div class="mt-36 lg:mt-44">
+            <p class="font-display text-base text-slate-900">Trusted by these six companies so far</p>
+            <ul
+                role="list"
+                class="mt-8 flex items-center justify-center gap-x-8 sm:flex-col sm:gap-x-0 sm:gap-y-10 xl:flex-row xl:gap-x-12 xl:gap-y-0"
+            >
+        {HERO_IMAGES
+            .into_iter()
+            .map(|group| {
+                view! {
+                <li>
+                    <ul
+                        role="list"
+                        class="flex flex-col items-center gap-y-8 sm:flex-row sm:gap-x-12 sm:gap-y-0">
+                        {group.into_iter()
+                            .map(|image| {
+                                view! {
+                                  <li class="flex">
+                                    <img src={image.logo} alt={image.name} />
+                                  </li>
+                                }
+                            })
+                            .collect_view()}
+                    </ul>
+                </li>
+                }})
+            .collect_view()}
+            </ul>
+            
+            job_postings.with(|data| {
+              
+                <h1>{{data}}</h1>
+              
+            })
+            </div>
+            </Container>
+            </Suspense>
+        }
 }
