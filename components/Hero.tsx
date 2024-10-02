7e88b009ba4795741ca0b4ef6e721e734d3f3e61
marcusp619 <marcusp619@gmail.com>
2024-10-02 19:51:45 -0400

    refactor: update to United Reason

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,7 +13,7 @@ export function Hero() {
   return (
     <Container className="pb-16 pt-20 text-center lg:pt-32">
       <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
-        Accounting{' '}
+        Recruiting{' '}
         <span className="relative whitespace-nowrap text-blue-600">
           <svg
             aria-hidden="true"
@@ -28,7 +28,7 @@ export function Hero() {
         for small businesses.
       </h1>
       <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
-        Most bookkeeping software is accurate, but hard to use. We make the
+        Most recruiting software is accurate, but hard to use. We make the
         opposite trade-off, and hope you don’t get audited.
       </p>
       <div className="mt-10 flex justify-center gap-x-6">
--- tailwindui-template/src/app/layout.tsx
+++ tailwindui-template/src/app/layout.tsx
@@ -6,8 +6,8 @@ import { type Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - TaxPal',
-    default: 'TaxPal - Accounting made simple for small businesses',
+    template: '%s - United Reason',
+    default: 'United Reason - Accounting made simple for small businesses',
   },
   description:
     'Most bookkeeping software is accurate, but hard to use. We make the opposite trade-off, and hope you don’t get audited.',
